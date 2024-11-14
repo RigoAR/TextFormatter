ba1a62c8a9fcb7e4f42b52935b861a86ae7064aa
RigoAR <rigobertoalvaradorayo@gmail.com>
2024-11-14 10:19:18 -0600

    User stories 1 & 3 where output format is plain & html

--- src/TextFile.java
+++ src/TextFile.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 public class TextFile
 {
     private ArrayList<String> fileContent;
@@ -7,4 +8,43 @@ public class TextFile
     {
         fileContent = new ArrayList<>();
     }
+    public void printPlainText()
+    {
+        for(int i = 0; i < fileContent.size(); i++)
+        {
+            System.out.println(fileContent.get(i));
+        }
+    }
+
+    public TextFile(ArrayList<String> data)
+    {
+        fileContent = data;
+    }
+
+    public void printHTML()
+    {
+        System.out.println("<html dir=\"ltr\" lang=\"en\">");
+        System.out.println("<head>");
+        for(int i = 0; i < fileContent.size(); i++)
+        {
+            System.out.println("<text=" + fileContent.get(i) + ">");
+        }
+        System.out.println("</head>");
+        System.out.println("</html>");
+    }
+
+    public void printMarkDown()
+    {
+        String[] options = {"**", "***", "<sub>", "~~", "_"};
+        Random rng = new Random();
+        int number = rng.nextInt(options.length);
+        System.out.println("#" + fileContent.get(0));
+        for(int i = 0; i < fileContent.size(); i++)
+        {
+            String option = options[number];
+            System.out.println(option + fileContent.get(i) + option);
+        }
+    }
+
+
 }
