beb564ab8735730dfbec777ae10e33731aa5e5de
RigoAR <rigobertoalvaradorayo@gmail.com>
2024-11-14 10:26:58 -0600

    Added Switch between formats

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        ArrayList<String> data = new ArrayList<>();
+        data.add("cneicn ");
+        data.add("437492754");
+        data.add("df7g6");
+        TextFile file= new TextFile(data);
+        //file.printPlainText();
+        //file.printHTML();
+        //file.printMarkDown();
+        int option = 0;
+        switch (option) {
+
+            case 0:
+                file.printPlainText();
+                break;
+
+            case 1:
+                file.printHTML();
+                break;
+
+            case 2:
+                file.printMarkDown();
+                break;
+            default:
+                file.printPlainText();
+
+        }
+
     }
 }
