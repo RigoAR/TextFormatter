78436e52d83efc5513bd7d6d37213f4663d27c88
RigoAR <rigobertoalvaradorayo@gmail.com>
2024-11-14 10:39:47 -0600

    added second variant HTMLFormat

---
+++ src/HTMLFormat.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class HTMLFormat implements iFormat
+{
+    @Override
+    public void format(ArrayList<String> data) {
+        System.out.println("<html dir=\"ltr\" lang=\"en\">");
+        System.out.println("<head>");
+        for(int i = 0; i < data.size(); i++)
+        {
+            System.out.println("<text=" + data.get(i) + ">");
+        }
+        System.out.println("</head>");
+        System.out.println("</html>");
+    }
+}
--- src/iFormat.java
+++ src/iFormat.java
@@ -3,4 +3,5 @@ import java.util.ArrayList;
 public interface iFormat
 {
     public void format(ArrayList<String> data);
+
 }
