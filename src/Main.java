9cdd79b11792f6db09bbaa52c9457cc6e21c99ee
RigoAR <rigobertoalvaradorayo@gmail.com>
2024-11-25 18:29:03 -0600

    Made it so I can run and test it.

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,53 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         ArrayList<String> data = new ArrayList<>();
-        data.add("cneicn ");
-        data.add("437492754");
-        data.add("df7g6");
-        TextFile file= new TextFile(data);
-        //file.printPlainText();
-        //file.printHTML();
-        //file.printMarkDown();
-        int option = 0;
-        switch (option) {
+        data.add("Hello World!");
+        data.add("This is a test document.");
 
+        TextFile file = new TextFile(data);
+
+        System.out.println("Select a format to apply:");
+        System.out.println("0 - Plain Text");
+        System.out.println("1 - HTML");
+        System.out.println("2 - Markdown");
+        System.out.println("3 - JSON");
+        System.out.println("4 - XML");
+
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+
+        switch (option) {
             case 0:
                 file.setFormat(new PlainTextFormat());
                 break;
-
             case 1:
                 file.setFormat(new HTMLFormat());
                 break;
-
             case 2:
                 file.setFormat(new MarkDownFormat());
                 break;
+            case 3:
+                file.setFormat(new JSONFormat());
+                break;
+            case 4:
+                file.setFormat(new XMLFormat());
+                break;
             default:
+                System.out.println("Invalid option. Defaulting to Plain Text.");
                 file.setFormat(new PlainTextFormat());
-
         }
+
         file.printFormatted();
 
+        System.out.println("Enter a filename to save:");
+        scanner.nextLine();
+        String filename = scanner.nextLine();
+        file.saveToFile(filename);
+
+        System.out.println("Process complete!");
+        scanner.close();
     }
 }
