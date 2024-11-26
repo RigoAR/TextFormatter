de7b4d4afd0a97e8e0ae3eaab8c69a498e9f01e0
RigoAR <rigobertoalvaradorayo@gmail.com>
2024-11-25 18:32:09 -0600

    Added a save function

--- src/HTMLFormat.java
+++ src/HTMLFormat.java
@@ -1,16 +1,33 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class HTMLFormat implements iFormat
-{
+public class HTMLFormat implements iFormat {
+
     @Override
     public void format(ArrayList<String> data) {
         System.out.println("<html dir=\"ltr\" lang=\"en\">");
         System.out.println("<head>");
-        for(int i = 0; i < data.size(); i++)
-        {
-            System.out.println("<text=" + data.get(i) + ">");
+        for (String line : data) {
+            System.out.println("<text=" + line + ">");
         }
         System.out.println("</head>");
         System.out.println("</html>");
+    }
+
+    @Override
+    public void save(ArrayList<String> data, String filename) {
+        try (FileWriter writer = new FileWriter(filename)) {
+            writer.write("<html dir=\"ltr\" lang=\"en\">\n");
+            writer.write("<head>\n");
+            for (String line : data) {
+                writer.write("<text=" + line + ">\n");
+            }
+            writer.write("</head>\n");
+            writer.write("</html>");
+            System.out.println("File saved as HTML: " + filename);
+        } catch (IOException e) {
+            System.out.println("Error saving file: " + e.getMessage());
+        }
     }
 }
