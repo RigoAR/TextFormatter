ae5d35c065bbd0b3dadb32a57c407460ec25e387
RigoAR <rigobertoalvaradorayo@gmail.com>
2024-11-25 19:33:44 -0600

    Added 3rd user story about checking console logging for real-time monitoring

--- Event Logger/src/Main.java
+++ Event Logger/src/Main.java
@@ -1,20 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         EventLogger logger = EventLogger.getInstance();
-        logger.setLogLevel(EventLogger.LogLevel.DEBUG);
 
-        logger.log(EventLogger.LogLevel.INFO, "Application started");
-        logger.log(EventLogger.LogLevel.DEBUG, "Debugging process...");
-        logger.log(EventLogger.LogLevel.ERROR, "An error occurred");
 
-        System.out.println("\nFull log history:");
+        logger.setLogLevel(EventLogger.LogLevel.INFO);
+
+
+        logger.log(EventLogger.LogLevel.INFO, "Application started.");
+        logger.log(EventLogger.LogLevel.DEBUG, "Debugging application.");
+        logger.log(EventLogger.LogLevel.ERROR, "An error occurred.");
+
+
+        System.out.println("Log history:");
         for (String log : logger.getLogHistory()) {
             System.out.println(log);
         }
 
-        System.out.println("\nRecent logs:");
-        for (String log : logger.getRecentLogs(2)) {
-            System.out.println(log);
-        }
+
+        logger.log(EventLogger.LogLevel.INFO, "Processing user input.");
+        logger.log(EventLogger.LogLevel.ERROR, "Error while processing request.");
     }
 }
