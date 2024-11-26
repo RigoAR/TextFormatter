dc2e713964866d8ec8e2d4cb3b7e271233d83335
RigoAR <rigobertoalvaradorayo@gmail.com>
2024-11-25 18:31:33 -0600

    I added a save function.

--- src/iFormat.java
+++ src/iFormat.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 
-public interface iFormat
-{
-    public void format(ArrayList<String> data);
-
+public interface iFormat {
+    void format(ArrayList<String> data);
+    void save(ArrayList<String> data, String filename);
 }
